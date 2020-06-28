cfb444898a3d2811dcb9cd00f2abbe68186544e1
ahmed <ahmed.sherif.fci@gmail.com>
2020-06-28 17:21:25 +0200

    setting eslint and nodemon cofiguration

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('Welcome to Library API');
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
-})
+});
